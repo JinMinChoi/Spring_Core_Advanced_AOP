f9350f18adcbd8cf91e44d1b066e85c9d61a5a72
JinMinChoi <jinmin2216@naver.com>
2022-04-23 17:06:41 +0900

    feat: Add AspectV2.java pointcut separation

---
+++ src/main/java/hello/aop/order/aop/AspectV2.java
@@ -0,0 +1,23 @@
+package hello.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Slf4j
+@Aspect
+public class AspectV2 {
+
+    //hello.aop.order 패키지와 하위 패키
+    @Pointcut("execution(* hello.aop.order..*(..))")
+    private void allOrder(){} //point cut signature
+
+    @Around("allOrder()")
+    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable { //어드바이스
+
+        log.info("[log] {}", joinPoint.getSignature()); //join point signature
+        return joinPoint.proceed();
+    }
+}
